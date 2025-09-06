1d3468fada4dccac3c0c02ca9d4313f3db76aa7b
wh1txn1ght1 <wh1txn1ght1@gmail.com>
2025-09-06 20:00:34 +0300

    Replacing the wrong file with the correct one StepOne.py

--- StepOne.py
+++ StepOne.py
@@ -1,4 +1,35 @@
+# step one
+print("Step one. Пример соединений")
+a = "Моё имя "
+b = "Александр"
+print(a+b)
+name = "Alexander"
+print(f"Твое имя {name}")
 
+# step two
+print("Step two. Формула ИМТ")
+weight = int(input("Введи свой вес (кг): "))
+height = int(input("Введи свой рост (см): "))
+imt = weight/(height/100)**2
+imt = round(imt, 1)
+print(f"Твой ИМТ равен {imt}")
+
+# step three
+print("step three. Математические операции")
+print("Введите число")
+number = int(input())
+print(f"Квадрат числа: {number**2}, Куб числа: {number**3}, Остаток при делении на 7 числа: {number%7}")
+
+# step four
+print("step four. Длина слова и символ")
+print("Введите слово")
+text = str(input())
+length = len(text)
+print("Длина слова ", length)
+if len(text)>3:
+    print("Четвертый символ ",text[3])
+else:
+    print("Слово слишком короткое.")
 #step five
 print("step five. Итог")
 age = int(input("Введи свой возраст: "))
