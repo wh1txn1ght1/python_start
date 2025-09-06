585c4f45b69f05c663a0262d7f34817befee8b94
wh1txn1ght1 <wh1txn1ght1@gmail.com>
2025-09-06 17:36:25 +0300

    Add one step file education StepOne.py

---
+++ StepOne.py
@@ -0,0 +1,10 @@
+
+#step five
+print("step five. Итог")
+age = int(input("Введи свой возраст: "))
+if age<18:
+    print("Ты еще подросток")
+elif age<40:
+    print("Ты взрослый")
+else:
+    print("Ты старший")
